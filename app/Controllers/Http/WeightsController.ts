06950cbb32d5fb1a963ab929ce65f20c4b157f46
Nathan Lemoine <nl024482@chbah.org>
2023-12-11 15:45:27 +0100

    Add weight management functionality to WeightsController

--- app/Controllers/Http/WeightsController.ts
+++ app/Controllers/Http/WeightsController.ts
@@ -1,17 +1,87 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import CreateWeightValidator from 'App/Validators/CreateWeightValidator'
+import Pet from 'App/Models/Pet'
+import Weight from 'App/Models/Weight'
+import UpdateWeightValidator from 'App/Validators/UpdateWeightValidator'
 
 export default class WeightsController {
-  public async index({}: HttpContextContract) {}
+  public async index({ params, response }: HttpContextContract) {
+    const pet = await Pet.findOrFail(params.pet_id)
 
-  public async create({}: HttpContextContract) {}
+    await pet.load('weights', (query) => {
+      query.select('id', 'weight', 'updated_at')
+    })
 
-  public async store({}: HttpContextContract) {}
+    return response.ok({
+      status: 200,
+      success: true,
+      message: 'Les poids ont bien été récupérés.',
+      data: pet.weights,
+    })
+  }
 
-  public async show({}: HttpContextContract) {}
+  public async store({ request, params, response }: HttpContextContract) {
+    const data = await request.validate(CreateWeightValidator)
 
-  public async edit({}: HttpContextContract) {}
+    const pet = await Pet.findOrFail(params.pet_id)
 
-  public async update({}: HttpContextContract) {}
+    const weight = new Weight()
+    weight.weight = data.weight
 
-  public async destroy({}: HttpContextContract) {}
+    await pet.related('weights').save(weight)
+
+    return response.created({
+      status: 201,
+      success: true,
+      message: 'Le poids a bien été créé.',
+      data: weight,
+    })
+  }
+
+  public async show({ response, params }: HttpContextContract) {
+    const data = await Weight.query()
+      .where('pet_id', params.pet_id)
+      .andWhere('id', params.id)
+      .firstOrFail()
+
+    return response.ok({
+      status: 200,
+      success: true,
+      message: 'Le poids a bien été récupéré.',
+      data: data,
+    })
+  }
+
+  public async update({ params, response, request }: HttpContextContract) {
+    const data = await request.validate(UpdateWeightValidator)
+    const weight = await Weight.query()
+      .where('pet_id', params.pet_id)
+      .andWhere('id', params.id)
+      .firstOrFail()
+
+    await weight.merge(data).save()
+
+    return response.ok({
+      status: 200,
+      success: true,
+      message: 'Le poids a bien été mis à jour.',
+      data: weight,
+    })
+  }
+
+  public async destroy({ params, response }: HttpContextContract) {
+    const weight = await Weight.query()
+      .where('pet_id', params.pet_id)
+      .andWhere('id', params.id)
+      .firstOrFail()
+
+    await weight.delete()
+
+    return response.ok({
+      status: 200,
+      success: true,
+      message: 'Le poids a bien été supprimé.',
+      data: weight,
+    })
+  }
 }
