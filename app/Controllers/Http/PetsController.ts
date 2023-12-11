36c58ac68bf7b8f497c90ce62dbeefa6a623c7b4
Nathan Lemoine <nl024482@chbah.org>
2023-12-11 15:45:21 +0100

    Refactor PetsController to include CRUD operations and validation

--- app/Controllers/Http/PetsController.ts
+++ app/Controllers/Http/PetsController.ts
@@ -1,25 +1,101 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import CreatePetValidator from 'App/Validators/CreatePetValidator'
+import UpdatePetValidator from 'App/Validators/UpdatePetValidator'
 import Pet from 'App/Models/Pet'
 
 export default class PetsController {
   public async index({ response }: HttpContextContract) {
-    const pets = await Pet.query().select('*').from('pets').preload('type').preload('weights')
+    const pets = await Pet.query()
+      .select('id', 'name', 'birth_date', 'type_id')
+      .preload('type', (query) => {
+        query.select('id', 'type')
+      })
 
     return response.ok({
       status: 200,
+      success: true,
+      message: 'Les animaux ont bien été récupérés.',
       data: pets,
     })
   }
 
-  public async create({}: HttpContextContract) {}
+  public async store({ auth, request, response }: HttpContextContract) {
+    const data = await request.validate(CreatePetValidator)
+    const pet = await Pet.create(data)
 
-  public async store({}: HttpContextContract) {}
+    await auth.user?.related('pets').attach({
+      [pet.id]: {
+        owner: true,
+      },
+    })
 
-  public async show({}: HttpContextContract) {}
+    return response.created({
+      status: 201,
+      success: true,
+      message: "L'animal a bien été créé.",
+      data: pet,
+    })
+  }
 
-  public async edit({}: HttpContextContract) {}
+  public async show({ response, params }: HttpContextContract) {
+    const pet = await Pet.query()
+      .preload('type', (query) => {
+        query.select('id', 'type')
+      })
+      .preload('weights', (query) => {
+        query.select('id', 'weight')
+      })
+      .where('id', params.id)
+      .firstOrFail()
 
-  public async update({}: HttpContextContract) {}
+    return response.ok({
+      status: 200,
+      success: true,
+      message: "L'animal a bien été récupéré.",
+      data: pet,
+    })
+  }
 
-  public async destroy({}: HttpContextContract) {}
+  public async update({ request, response, params }: HttpContextContract) {
+    const data = await request.validate(UpdatePetValidator)
+    const pet = await Pet.findOrFail(params.id)
+
+    await pet.merge(data).save()
+
+    return response.ok({
+      status: 200,
+      success: true,
+      message: "L'animal a bien été mis à jour.",
+      data: pet,
+    })
+  }
+
+  public async destroy({ params, response }: HttpContextContract) {
+    const pet = await Pet.findOrFail(params.id)
+
+    await pet.delete()
+
+    return response.ok({
+      status: 200,
+      success: true,
+      message: "L'animal a bien été supprimé.",
+    })
+  }
+
+  public async follow({ auth, params }: HttpContextContract) {
+    const user = auth.user!
+    const pet = await Pet.findOrFail(params.id)
+
+    await user.related('pets').attach({
+      [pet.id]: {
+        owner: false,
+      },
+    })
+
+    return {
+      status: 200,
+      success: true,
+      message: `Vous suivez ${pet.name}.`,
+    }
+  }
 }
