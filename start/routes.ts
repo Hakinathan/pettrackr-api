aa81a3b8a557f86b238874b3a94a044f39f7bbe7
Nathan Lemoine <nl024482@chbah.org>
2023-12-11 15:43:17 +0100

    Refactor routes file to use Route.resource for pets and weights

--- start/routes.ts
+++ start/routes.ts
@@ -1,23 +1,3 @@
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| This file is dedicated for defining HTTP routes. A single file is enough
-| for majority of projects, however you can define routes in different
-| files and just make sure to import them inside this file. For example
-|
-| Define routes in following two files
-| ├── start/routes/cart.ts
-| ├── start/routes/customer.ts
-|
-| and then import them inside `start/routes.ts` as follows
-|
-| import './routes/cart'
-| import './routes/customer'
-|
-*/
-
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
@@ -29,8 +9,6 @@ Route.group(() => {
   .as('auth')
 
 Route.group(() => {
-  Route.get('/pets', 'PetsController.index').as('index')
+  Route.resource('pets', 'PetsController').apiOnly().as('pets')
   Route.resource('pets.weights', 'WeightsController').apiOnly().as('weights')
-})
-  // .middleware('auth')
-  .as('pets')
+}).middleware('auth')
